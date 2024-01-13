57c2201df2dd5d008ec942d967f24874a0c76ded
felipe239989 <felipeanselmodonascimento@gmail.com>
2024-01-13 09:06:20 -0300

    outro contrato

--- src/components/Section2.tsx
+++ src/components/Section2.tsx
@@ -12,7 +12,7 @@ export function Section2() {
 
     const handleCopyClick = () => {
       // Texto que você deseja copiar
-      const textToCopy = '0x6262deE58Ea6d4fa1EB12b1e9ae8ECCD4361FF02';
+      const textToCopy = '0x43e2aA014BbE903B8f2D5E0ec68C0F054612D9E8';
   
       // Copia o texto para a área de transferência
       navigator.clipboard.writeText(textToCopy)
@@ -49,7 +49,7 @@ export function Section2() {
                 <div className={styles.divsec}>
                     <h1>ABOUT LONG GOOSE COIN!</h1>
                     <p>LongGoose Coin is the cryptocurrency of the future, driving the financial revolution with cutting-edge technology. Enjoy fast, efficient, and secure transactions. Our community-driven governance, focus on financial inclusion, and advanced smart contracts make LongGoose Coin the choice for a more connected world. Discover the power of innovation - acquire LongGoose Coin now! 💸🚀🌐</p>
-                    <button onClick={handleCopyClick} role="button">CONTRACT:0x6262deE58Ea6d4fa1EB12b1e9ae8ECCD4361FF02
+                    <button onClick={handleCopyClick} role="button">CONTRACT:0x43e2aA014BbE903B8f2D5E0ec68C0F054612D9E8
                     <Copy size={15} /></button>
                 </div>
             </div>
